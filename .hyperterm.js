8182b6b911626afd91115c7502eb92c7f9064f6b
David Ratajczak <david@mockra.com>
2016-09-26 17:39:57 -0700

    updates color theme for hyperterm

--- .hyperterm.js
+++ .hyperterm.js
@@ -58,7 +58,7 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: ['hypercwd',
-    'hyperterm-atom-dark',
+    'nova-hyperterm',
     'hyperterm-tab-icons'],
 
   // in development, you can create a directory under
