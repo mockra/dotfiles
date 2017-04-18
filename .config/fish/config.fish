b799b1e9a728f8f8adb842c1e620f8be7ec1d31f
David Ratajczak <david@mockra.com>
2017-04-17 17:31:20 -0700

    Clean up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,4 @@
 set -gx OMF_PATH $HOME/.local/share/omf
-source $OMF_PATH/init.fish
-
-set -g fish_greeting ''
-
-function fish_title
-  git rev-parse --abbrev-ref HEAD
-end
-funcsave fish_title
 
 alias g='git'
 alias gd='g diff'
@@ -29,6 +21,3 @@ set -gx PATH $HOME/.rbenv/shims /usr/local/bin /usr/bin /bin /usr/sbin /sbin $PA
 set -x NODE_PATH /usr/local/lib/node_modules
 
 rbenv rehash >/dev/null ^&1
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-test -s {$HOME}/.kiex/scripts/kiex.fish ; and source {$HOME}/.kiex/scripts/kiex.fish
