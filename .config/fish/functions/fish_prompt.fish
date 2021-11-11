8b1cff0d3680c39f622e01c4532550b0f29378bf
David Ratajczak <david@mockra.com>
2021-11-10 20:11:09 -0800

    Add tide fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,48 +1,38 @@
 function fish_prompt
-  # Last status code if not successful
-  set --local last_status $status
-  if [ $last_status -gt 0 ]
-    echo -n "Non-zero exit code: "
-    set_color --bold red
-    echo $last_status
-    set_color normal
-  end
+end
 
-  # I like an extra new line between commands
-  echo
+status is-interactive || exit
 
-  # Print current folder, `~` if $HOME
-  set_color --bold blue
-  echo -n (basename (string replace -r "^$HOME" "~"  $PWD))"/"
-  set_color normal
+_tide_remove_unusable_items
+_tide_cache_variables
 
-  # Git time. First lets find out if we're even in a git repo
-  set --local git_status (/usr/bin/env git status 2>/dev/null)
-  if [ "$git_status" ]
-    # We are! Lets print some branch info, colored based on tree dirtiness
-    set --local branch (/usr/bin/env git symbolic-ref HEAD 2>/dev/null | awk -F/ '{print $NF}')
-    set --local clean (echo $git_status | tail -n 1 | grep "nothing to commit, working tree clean")
-    set --local unpushed (/usr/bin/env git cherry -v origin/$branch 2>/dev/null)
+# The first element in $$_tide_prompt_var is right prompt
+# All remaining ones are 'left' prompt (also upper right in 2-line prompts)
+set -g _tide_prompt_var _tide_prompt_$fish_pid
 
-    echo -n " on "
+function _tide_refresh_prompt --on-variable $_tide_prompt_var
+    set -g _tide_self_repainting # prevents us from creating a second background job
+    commandline --function repaint
+end
 
-    if [ "$clean" ]
-      set_color --bold green
-    else
-      set_color --bold red
+function fish_prompt
+    _tide_last_status=$status _tide_last_pipestatus=$pipestatus if not set -e _tide_self_repainting
+        jobs --query
+        fish --command "_tide_jobs_status=$status CMD_DURATION=$CMD_DURATION COLUMNS=$COLUMNS \
+            fish_bind_mode=$fish_bind_mode set -U $_tide_prompt_var (_tide_prompt)" &
+        builtin disown
+
+        command kill $_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid $last_pid
     end
 
-    echo -n $branch
-    set_color normal
+    string unescape $_tide_add_newline $$_tide_prompt_var[1][2..]
+end
 
-    # Now, lets check for and announce unpushed commits
-    if [ "$unpushed" ]
-      echo -n " with "
-      set_color --bold magenta
-      echo -n "unpushed"
-      set_color normal
-    end
-  end
+function fish_right_prompt
+    string unescape $$_tide_prompt_var[1][1]
+end
 
-  echo " › "
+function _tide_on_fish_exit --on-event fish_exit
+    set -e $_tide_prompt_var
 end
