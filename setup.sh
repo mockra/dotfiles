b2ae77b7c5c5bf2b950156ab2b3490f4cb438c3a
David Ratajczak <david@mockra.com>
2022-01-21 13:12:27 -0800

    Add mkdir -p

--- setup.sh
+++ setup.sh
@@ -10,15 +10,15 @@ sudo apt-get update
 sudo apt-get --assume-yes install silversearcher-ag fish tmux neovim exa
 
 ## Link Dotfiles
-mkdir $HOME/.config/nvim
-mkdir $HOME/.config/fish
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/fish
 
-ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/.tmux-status.conf $HOME/.tmux-status.conf
+ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -sf $(pwd)/.tmux-status.conf $HOME/.tmux-status.conf
 
-ln -s $(pwd)/.config/nvim/* $HOME/.config/nvim/
-ln -s $(pwd)/.vimrc $HOME/.vimrc
-ln -s $(pwd)/.gitconfig $HOME/.gitconfig
+ln -sf $(pwd)/.config/nvim/* $HOME/.config/nvim/
+ln -sf $(pwd)/.vimrc $HOME/.vimrc
+ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
 
 rm -f $HOME/.zshrc
 
