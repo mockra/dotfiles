e755aafe1c0dc222974eef05f19b898421e3be4e
David Ratajczak <david@mockra.com>
2022-06-29 12:14:20 -0700

    Update setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ set -x
 sudo apt-get update -y
 sudo apt-get --assume-yes install silversearcher-ag tmux neovim
 
+rm -f $HOME/.zshrc
+ln -sf $(pwd)/.zshrc $HOME/.zshrc
+
 rm -rf $HOME/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
 $HOME/.fzf/install --all
@@ -22,14 +25,9 @@ ln -sf $(pwd)/.config/nvim/* $HOME/.config/nvim/
 ln -sf $(pwd)/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
 
-rm -f $HOME/.zshrc
-ln -sf $(pwd)/.zshrc $HOME/.zshrc
-
 ### Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ### Install NeoVim plugins
 nvim --headless +PlugInstall +qa
-
-chsh -s $(which zsh)
