d49c05c954c0412987afc470d7577134b1fa4c14
David Ratajczak <david@mockra.com>
2023-09-01 17:34:13 -0700

    Add wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Tokyo Night Moon'
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font 'Cartograph CF'
+
+config.keys = {
+  {
+    key = 'LeftArrow',
+    mods = 'SUPER',
+    action = wezterm.action.ActivateTabRelative(-1)
+  },
+  {
+    key = 'RightArrow',
+    mods = 'SUPER',
+    action = wezterm.action.ActivateTabRelative(1)
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
