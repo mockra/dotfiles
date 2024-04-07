ef1e4cf638e441d1a29e7960d08ab65e1ed06aff
David Ratajczak <david@mockra.com>
2024-04-07 08:47:14 -0700

    Add lazy sync

--- setup.sh
+++ setup.sh
@@ -47,6 +47,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ### Install NeoVim plugins
-nvim --headless +PlugInstall +qa
+nvim --headless "+Lazy! sync" +qa
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
