acb45e0dbb17d7893428ae6855edf1fe734fc037
David Ratajczak <david@mockra.com>
2024-04-07 08:45:38 -0700

    Working nvim-cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,19 +21,21 @@ require("lazy").setup({
   "neovim/nvim-lspconfig",
   "nvim-lua/plenary.nvim",
 
-   "williamboman/mason.nvim",
-   "williamboman/mason-lspconfig.nvim",
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
-
   -- Find/replace
   "nvim-pack/nvim-spectre",
 
   -- Autocomplete
   -- "ervandew/supertab",
+
+  -- Autocomplete
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-vsnip',
+  'hrsh7th/vim-vsnip',
+  'hrsh7th/cmp-git',
 
   -- Basic vim tools
   "tpope/vim-sensible",
@@ -94,153 +96,6 @@ require("lazy").setup({
   },
 })
 
--- Key mappings
-require('mapx').setup{ global = true }
-
-map("<leader>bi", ":!bundle install<cr>")
-map("<leader>v", ":vsplit<cr>")
-map("<leader>a", ":A<cr>")
-map("<leader>w", ":w<cr>")
-map("<leader>g", ":%s/\\<<C-r><C-w>\\>/")
-map("<leader>p", ":r !pbpaste<cr>")
-map("<leader>D", ":call delete(expand('%')) \\| bdelete!<cr>")
-map("<leader>s", ":%sort<cr>")
-map("<leader>q", ":q<cr>")
-map("<leader>h", ":s/:\\([^ ]*\\)\\(\\s*\\)=>/\\1:/g<cr>")
-map("<leader>o", ":Goyo<cr>")
-vnoremap("<leader>y", ":OSCYankVisual<CR>")
-nnoremap("<leader>yf", ":let @*=expand('%')<CR>")
-
-vnoremap("<leader>s", "<esc>:lua require('spectre').open_visual()<CR>")
-
-nmap("<leader>t", ":w | :TestNearest | :VimuxZoomRunner<CR>", "silent")
-map("<leader>T", ":w | :TestFile | :VimuxZoomRunner<CR>", "silent")
-nmap(":W", ":w")
-nmap(":W!", ":w!")
-nmap(":Q", ":q")
-nmap(":Q!", ":q!")
-nmap(":Wq!", ":wq!")
-nmap(":WQ!", ":wq!")
-
-map("<leader>f", ":Telescope live_grep<cr>")
-nnoremap("go", ":Telescope find_files shorten_path=true<CR>", "silent")
-nnoremap("gr", ":Telescope oldfiles<CR>", "silent")
-
-map("<Leader>z", ":VimuxZoomRunner<CR>")
-
-vim.g.VimuxHeight = "10"
-vim.g.ruby_path = "/Users/mockra/.rbenv/shims/ruby"
-
-vim.o["test#javascript#mocha#options"] = "-A --compilers js:babel-register"
-vim.o["test#strategy"] = "vimux"
-vim.o["test#ruby#rspec#executable"] = "script/test"
-
-vim.g.ruby_indent_block_style = 'do'
-vim.g.ruby_indent_assignment_style = 'variable'
-
-vim.g.tokyonight_style = "moon"
-vim.g.tokyonight_enable_italic = 1
-vim.g.tmuxline_preset = "nightly_fox"
-
-vim.g.lightline = { colorscheme = 'tokyonight' }
-
-vim.cmd [[colorscheme tokyonight-moon]]
-vim.cmd [[set termguicolors]]
-vim.cmd [[set shell=bash]]
-vim.cmd [[set encoding=utf-8]]
-vim.cmd [[set nocompatible]]
-
-vim.cmd("set t_Co=256")
-vim.cmd("set cc=+1")
-vim.cmd("set cursorline")
-
-vim.cmd("set nobackup")
-vim.cmd("set nowritebackup")
-vim.cmd("set noswapfile")
-vim.cmd("set history=50")
-vim.cmd("set ruler")
-vim.cmd("set showcmd")
-vim.cmd("set incsearch")
-vim.cmd("set laststatus=2")
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set smarttab")
-vim.cmd("set expandtab")
-vim.cmd("set hlsearch")
-vim.cmd("set list listchars=tab:»·,trail:·")
-vim.cmd("set lazyredraw")
-
-vim.cmd("set textwidth=109")
-vim.cmd("set backspace=2")
-vim.cmd("set ignorecase smartcase")
-
-vim.cmd("set number")
-vim.cmd("set numberwidth=5")
-
-vim.cmd("set wildmode=list:longest,list:full")
-vim.cmd("set complete=.,w,t")
-vim.cmd("set wildignore+=*/tmp/*,*.so,*.swp,*.zip,*/vendor/*,*/node_modules/*")
-
-vim.cmd("set inccommand=split")
-
-map("<leader>n", ":Rename ")
-
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
-
--- to learn how to use mason.nvim with lsp-zero
--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'ruby_ls'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
-})
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
--- this is the function that loads the extra snippets to luasnip
--- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
-
-cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  formatting = lsp_zero.cmp_format({details = false}),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-})
+require("settings")
+require("keymaps")
+require("configs/cmp")
