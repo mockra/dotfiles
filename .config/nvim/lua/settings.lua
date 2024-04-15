a255a690175f6095dc1e224e6500d0272291f100
David Ratajczak <david@mockra.com>
2024-04-14 19:16:07 -0700

    Update jest executable

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -5,6 +5,8 @@ vim.g["test#javascript#mocha#options"] = "-A --compilers js:babel-register"
 vim.g["test#strategy"] = "vimux"
 vim.g["test#ruby#rspec#executable"] = "script/test"
 vim.g["test#javascript#runner"] = 'jest'
+-- Configure jest for ESM modules
+vim.g["test#javascript#jest#executable"] = 'node --experimental-vm-modules node_modules/jest/bin/jest.js'
 
 vim.g.ruby_indent_block_style = 'do'
 vim.g.ruby_indent_assignment_style = 'variable'
