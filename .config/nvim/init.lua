f49dbe9412435f9662840d0cd816d576aecbafb3
David Ratajczak <david@mockra.com>
2025-10-10 10:25:39 -0700

    Fix vim-tmux-navigator for sidekick

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -167,7 +167,36 @@ require("lazy").setup({
   -- Testing
   "janko-m/vim-test",
   "benmills/vimux",
+
+  -- Navigation between tmux and vim splits
   "christoomey/vim-tmux-navigator",
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { mode = 'n', '<c-h>', '<cmd>TmuxNavigateLeft<cr>' },
+      { mode = 'n', '<c-j>', '<cmd>TmuxNavigateDown<cr>' },
+      { mode = 'n', '<c-k>', '<cmd>TmuxNavigateUp<cr>' },
+      { mode = 'n', '<c-l>', '<cmd>TmuxNavigateRight<cr>' },
+      { mode = 'n', '<c-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
+      { mode = 't', '<c-h>', '<C-w><cmd>TmuxNavigateLeft<cr>' },
+      { mode = 't', '<c-j>', '<C-w><cmd>TmuxNavigateDown<cr>' },
+      { mode = 't', '<c-k>', '<C-w><cmd>TmuxNavigateUp<cr>' },
+      { mode = 't', '<c-l>', '<C-w><cmd>TmuxNavigateRight<cr>' },
+      { mode = 't', '<c-\\>', '<C-w><cmd>TmuxNavigatePrevious<cr>' },
+    },
+    init = function()
+      -- Disable default mappings to make it work in terminal mode
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+  },
 
   -- Language stuff
   "tpope/vim-rails",
